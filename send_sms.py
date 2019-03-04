90543ed6f6b5fe999569518ecbbb0a239afbb517
Katherine Hartling <katherinehartling@Katherines-MacBook.local>
2019-03-04 15:46:09 -0500

    Environment Variables set up

--- send_sms.py
+++ send_sms.py
@@ -1,14 +1,13 @@
 import os
 from twilio.rest import Client
 
-account_sid = os.getenv["TWILIO_ACCOUNT_SID"]
-auth_token = os.getenv["TWILIO_AUTH_TOKEN"]
-
+account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
+auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
 
 client = Client(account_sid, auth_token)
 
 client.messages.create(
 	to=os.environ["MY_PHONE_NUMBER"],
 	from_="+14159806136",
-	body="Hi Diffles I wrote this script with Twilio to text you"
+	body="This is working"
 )
